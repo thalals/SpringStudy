4542c144d0800f9ea53231a08a9eb40c2e1a8984
Hyeongmin Park <thalsal@naver.com>
2023-11-02 16:27:40 +0900

    init : spring boot embedded mongo db 설정

---
+++ inmemory-mongodb-for-test/src/main/java/com/inmemorymongodbfortest/inmemorymongodbfortest/InmemoryMongodbForTestApplication.java
@@ -0,0 +1,13 @@
+package com.inmemorymongodbfortest.inmemorymongodbfortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmemoryMongodbForTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmemoryMongodbForTestApplication.class, args);
+	}
+
+}
---
+++ inmemory-mongodb-for-test/src/test/java/com/inmemorymongodbfortest/inmemorymongodbfortest/InmemoryMongodbForTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmemorymongodbfortest.inmemorymongodbfortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmemoryMongodbForTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
