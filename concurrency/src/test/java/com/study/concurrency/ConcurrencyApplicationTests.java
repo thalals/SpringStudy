1f6d3ceb7662ec3e9c220cd4ce3dfcda375ac3b8
thalals <thalsal@naver.com>
2024-06-10 17:42:16 +0900

    init concurrency study project

---
+++ concurrency/src/test/java/com/study/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
