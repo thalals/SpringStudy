cb52c128a9c5dd34249ad1a410c7ae8e77a83b6e
hyungmin-gn <hyungmin.park@genesisnest.com>
2024-10-10 10:03:45 +0900

    :tada: init

---
+++ shedlock/src/main/java/com/example/shedlock/ShedlockApplication.java
@@ -0,0 +1,13 @@
+package com.example.shedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShedlockApplication.class, args);
+	}
+
+}
---
+++ shedlock/src/test/java/com/example/shedlock/ShedlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
