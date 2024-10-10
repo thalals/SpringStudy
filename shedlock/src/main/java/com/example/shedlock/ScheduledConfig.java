b27506681bc352fc73770c0bb916205a811f7d67
thalals <thalsal@naver.com>
2024-10-10 16:19:32 +0900

    :heavy_plus_sign: config shedlock

--- shedlock/src/main/java/com/example/shedlock/ScheduledConfig.java
+++ shedlock/src/main/java/com/example/shedlock/ScheduledConfig.java
@@ -1,15 +1,35 @@
 package com.example.shedlock;
 
+import javax.sql.DataSource;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider.ColumnNames;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10S")
 public class ScheduledConfig {
 
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+            JdbcTemplateLockProvider.Configuration.builder()
+                .withTableName("shed_lock_t")
+                .withColumnNames(new ColumnNames("task_name", "lock_until_tmstamp", "locked_at_tmstamp", "locked_by"))
+                .withJdbcTemplate(new JdbcTemplate(dataSource))
+                .usingDbTime()
+                .build()
+        );
+    }
+
+    //스케줄러 멀티 스레드로 실행
     @Bean
     public TaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
@@ -21,3 +41,4 @@ public class ScheduledConfig {
         return scheduler;
     }
 }
+
