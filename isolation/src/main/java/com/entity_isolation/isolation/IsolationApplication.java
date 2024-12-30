4cfe3d2fd060f744c9876fbb1459b230c3127a0f
thalals <thalsal@naver.com>
2024-12-30 23:17:45 +0900

    init : setting project

---
+++ isolation/src/main/java/com/entity_isolation/isolation/IsolationApplication.java
@@ -0,0 +1,13 @@
+package com.entity_isolation.isolation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsolationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsolationApplication.class, args);
+	}
+
+}
---
+++ isolation/src/test/java/com/entity_isolation/isolation/IsolationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entity_isolation.isolation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsolationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
