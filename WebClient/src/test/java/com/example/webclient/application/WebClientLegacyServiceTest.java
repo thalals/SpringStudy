09700ba4dc4618e47795a200ff89d636d043e65d
박형민 <thalsal@gentlemonster.com>
2025-01-14 18:47:45 +0900

    test: dump

--- WebClient/src/test/java/com/example/webclient/application/WebClientLegacyServiceTest.java
+++ WebClient/src/test/java/com/example/webclient/application/WebClientLegacyServiceTest.java
@@ -1,8 +1,10 @@
 package com.example.webclient.application;
 
-import mockwebserver3.MockWebServer;
+import com.example.webclient.util.WebClientUtil;
 import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -54,8 +56,98 @@ class WebClientLegacyServiceTest {
         String bodyJson = """
                 {
                  "result": true,
-                 "message": "",
+                 "message": ""
                  }
+            """;
+
+        MockResponse mockResponse = new MockResponse()
+            .setBody(bodyJson)
+            .addHeader("Content-Type", "application/json")
+            .setResponseCode(200);
+
+        mockWebServer.enqueue(mockResponse);
+
+        when(WebClientUtil.getBaseUrl(any())).thenReturn(mockWebClient);
+
+        // when
+        boolean result = webClientLegacyService.callWebClient();
+
+        // then
+        Assertions.assertTrue(result);
+
+    }
+
+    @Test
+    @DisplayName("자사몰 환불 요청 실패 테스트 - 400 error")
+    void refund_fail_for400() {
+
+        // given
+        String bodyJson = """
+                {
+                 "result": false
+                 }
+            """;
+
+        MockResponse mockResponse = new MockResponse()
+            .setBody(bodyJson)
+            .addHeader("Content-Type", "application/json")
+            .setResponseCode(400);
+
+        mockWebServer.enqueue(mockResponse);
+
+        when(webService.webClient(any())).thenReturn(mockWebClient);
+
+        // when
+        Delivery delivery = DeliveryFixture.create();
+
+        // then
+        assertThatThrownBy(() -> orderEcomService.refund(delivery))
+            .isInstanceOf(EcomApiException.class)
+            .hasMessage("4xx 자사몰 시스템 호출 오류.");
+
+    }
+
+    @Test
+    @DisplayName("자사몰 환불 요청 실패 테스트 - 500 error")
+    void refund_fail_for500() {
+
+        // given
+        String bodyJson = """
+                {
+                 "result": false
+                 }
+            """;
+
+        MockResponse mockResponse = new MockResponse()
+            .setBody(bodyJson)
+            .addHeader("Content-Type", "application/json")
+            .setResponseCode(500);
+
+        mockWebServer.enqueue(mockResponse);
+
+        when(webService.webClient(any())).thenReturn(mockWebClient);
+
+        // when
+        Delivery delivery = DeliveryFixture.create();
+
+        // then
+        assertThatThrownBy(() -> orderEcomService.refund(delivery))
+            .isInstanceOf(EcomApiException.class)
+            .hasMessage("5xx 자사몰 시스템 호출 오류.");
+    }
+
+    @Test
+    @DisplayName("자사몰 환불 요청 실패 테스트 - 결과 false")
+    void refund_fail() {
+
+        // given
+        String bodyJson = """
+                {
+                  "result" : false,
+                  "status" : 400,
+                  "message" : "refund error",
+                  "subCode" : "INVALID_ACCESS"
+                }
             """;
 
         MockResponse mockResponse = new MockResponse()
@@ -69,11 +161,11 @@ class WebClientLegacyServiceTest {
 
         // when
         Delivery delivery = DeliveryFixture.create();
-        Boolean refund = orderEcomService.refund(delivery);
 
         // then
-        Assertions.assertTrue(refund);
-
+        assertThatThrownBy(() -> orderEcomService.refund(delivery))
+            .isInstanceOf(EcomApiException.class)
+            .hasMessage("refund error");
     }
 
 }
