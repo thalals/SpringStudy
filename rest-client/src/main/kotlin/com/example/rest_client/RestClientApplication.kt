c62440235a86716baa4567c94b9eeef19629e697
thalals <thalsal@naver.com>
2025-03-04 22:46:52 +0900

    init : setting project

---
+++ rest-client/src/main/kotlin/com/example/rest_client/RestClientApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rest_client
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestClientApplication>(*args)
+}
